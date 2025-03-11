c4839cde71dc578998a079fbe498ea059980c825
danielsanchez18 <dsanchez151r@gmail.com>
2025-03-11 02:18:05 -0500

    changes to the home page

--- src/app/layouts/client/pages/menu/menu-page.component.ts
+++ src/app/layouts/client/pages/menu/menu-page.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
 })
 export class ClientMenuPageComponent {
 
+  categories = [
+    { id: 1, name: 'Desayunos' },
+    { id: 2, name: 'Almuerzos' },
+    { id: 3, name: 'Cenas' },
+    { id: 4, name: 'Snacks' },
+    { id: 5, name: 'Bebidas' },
+    { id: 6, name: 'Postres' },
+    { id: 7, name: 'Sandwiches' },
+    { id: 8, name: 'Hamburguesas' },
+    { id: 9, name: 'Ensaladas' },
+  ]
+
 }
